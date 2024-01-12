79143e9866419442c2cc5b3d9562c02034d4ab79
lpilp <lpilp@126.com>
2024-01-12 17:24:25 +0800

    添加sm4

---
+++ example/tsm4.php
@@ -0,0 +1,35 @@
+<?php
+require_once '../vendor/autoload.php';
+use Lpilp\Splsm2\smecc\SPLSM2\SimpleSm4;
+
+$key = "01234$678*abcde)";
+$iv = '1234567887654321';
+
+$sm4 = new SimpleSm4($key);
+
+$data = '我1爱你ILOVEYOU!!!';
+$data = str_repeat('abc',7);
+//sm4 的ecb 与cbc加密有补齐（16*n）l
+// sm4->encrypt($data, $type = 'sm4', $iv = '', $format = 'hex')
+//  openssl_encrypt ,和服务器openssl版本，PHP版本有关，有些服务器可能不支持sm4-* 相关的对称加密算法，
+
+
+echo "==== test sm4 sm4-cbc============";
+echo "\nphp sm4:     ".$hex = $sm4->encrypt($data,'sm4',$iv); //default is cbc
+echo "\nphp decode:  ".$sm4->decrypt($hex,'sm4',$iv,'hex');
+
+echo "\n==== test sm4-ecb============";
+echo "\nphp sm4-ecb: ".$hex = $sm4->encrypt($data,'sm4-ecb');
+echo "\nphp decode:  ".$sm4->decrypt($hex,'sm4-ecb','','hex');
+
+echo "\n==== test sm4-ofb============";
+echo "\nphp sm4-ofb: ".$hex = $sm4->encrypt($data,'sm4-ofb',$iv);
+echo "\nphp decode:  ".$sm4->decrypt($hex,'sm4-ofb',$iv,'hex');
+
+echo "\n==== test sm4-cfb============";
+echo "\nphp sm4-cfb: ".$hex = $sm4->encrypt($data,'sm4-cfb',$iv);
+echo "\nphp decode:  ".$sm4->decrypt($hex,'sm4-cfb',$iv,'hex');
+
+echo "\n==== test sm4-ctr============";
+echo "\nphp sm4-ctr: ".$hex = $sm4->encrypt($data,'sm4-ctr',$iv);
+echo "\nphp decode:  ".$sm4->decrypt($hex,'sm4-ctr',$iv,'hex');
--- example/tsplsm2.php
+++ example/tsplsm2.php
@@ -14,8 +14,7 @@ var_dump($s1 == $s2);
 $publicKey = '04eb4b8bbe15e3ad94b85196adc2c6f694436b3c1336170fd1daac8b10d2b8824ada9687c138fb81590e0f66ab9678161732ac0d7866b169e76b74483285f2bc04';
 $privateKey = '0bc1c1d2771b64ba1922d72f8a451cd09a82176f74d975d484ec62c862176b75';
 $userId = '1234567812345678';
-$document = "app_id=test221124213123300000012";
-// $document .= "xxxxdxx";
+
 $document = "我爱你ILOVEYOU!";
 $ssm2 = new SimpleSm2($privateKey,$publicKey);
 // 不随机变,就是固定的文本，固定的密钥，生成的固定的签名,
